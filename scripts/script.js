c702edcacb91c56d9df8d4abe68ac7d12025151b
Nick Prokofjev <nprockofjev@yandex.ru>
2020-08-23 16:13:52 +0400

    Finished startCards

--- scripts/script.js
+++ scripts/script.js
@@ -42,3 +42,61 @@ function editProfile(evt) {
 
 popupSaveButton.addEventListener('click', editProfile);
 
+
+//1. Выбрал блок добавления элементов
+const elementList = document.querySelector('.elements__list');
+
+//2. Определяем массив с карточками.
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+//3. Создадим функцию стартового добавления карточек в блок.
+
+function addCardsStart() {
+  //Определяем каждый элемент со свойствами в имеющемся массиве,
+  //и с помощью новой функции добавляем последовательно в конец секции
+  // с карточками новые элементы со свойствами из массива.
+  initialCards.forEach(function (element) {
+    // Определяем свойства
+    const cardLink = element.link;
+    const cardName = element.name;
+    //функция присваивания свойств карточке
+    function CardInfo(link, name) {
+      const elementCardTemplate = document.querySelector('#element').content.cloneNode(true);
+      console.log(elementCardTemplate);
+
+      elementCardTemplate.querySelector('.element__image').src = link;
+      elementCardTemplate.querySelector('.element__image').alt = 'Фото' + name;
+      elementCardTemplate.querySelector('.element__name').textContent = name;
+
+      return elementCardTemplate;
+      }
+    // Добавляем карточки в секцию
+    elementList.append(CardInfo(cardLink, cardName));
+  })
+}
+addCardsStart();
